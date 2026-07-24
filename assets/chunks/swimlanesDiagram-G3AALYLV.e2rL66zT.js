75e71847b99c8c7dc8dc4accd7b8bc68073954e1
DevinNorgarb <DevinNorgarb@users.noreply.github.com>
2026-07-24 01:32:55 +0000

    deploy: 4276b38ed820603bb447f579cb40571dc375ce17

---
+++ assets/chunks/swimlanesDiagram-G3AALYLV.e2rL66zT.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.CFSd_nP1.js";import{_ as a}from"../app.DVunwe9b.js";import"./chunk-5VM5RSS4.C-keBn0q.js";import"./chunk-XXDRQBXY.Cs7vs-1A.js";import"./chunk-VR4S4FIN.DogUTWsO.js";import"./chunk-32BRIVSS.0rPxrYp4.js";import"./channel.C9NDMV_5.js";import"./framework.Crpw6aS5.js";import"./theme.Di61xxMM.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
